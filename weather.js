88cade8def5bfd50dff69b806cbe2d6147408d7b
erol1098 <erol1098@gmail.com>
2022-06-15 13:45:57 +0300

    Initial Commit

---
+++ weather.js
@@ -0,0 +1,119 @@
+"use strict";
+
+const key = "b473abf08211233160d13b09b0646297";
+const dataArr = [];
+const city = document.querySelector(".entry");
+const search = document.getElementById("btn");
+const content = document.querySelector(".content");
+search.addEventListener("click", (e) => {
+  fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city.value}&limit=5&appid=b473abf08211233160d13b09b0646297`
+  )
+    .then((res) => {
+      if (!res.ok) throw new Error("Something Went Wrong");
+
+      return res.json();
+    })
+    .then((data) => {
+      //   const cities = data.map((city) => {
+      //     const { name, lat, lon, country, state } = city;
+      //     return {
+      //       name: name,
+      //       country: country,
+      //       state: state,
+      //       lat: lat,
+      //       lon: lon,
+      //     };
+      //   });
+      //   console.log(cities);
+      //   const table = document.createElement("table");
+      //   table.classList.add("table");
+      //   const title = document.createElement("thead");
+      //   title.innerHTML = `
+      //   <tr>
+      //   <th scope="col">#</th>
+      //   <th scope="col">City</th>
+      //   <th scope="col">Country</th>
+      //   <th scope="col">State</th>
+      // </tr>
+      // `;
+      //   table.append(title);
+
+      //   cities.forEach((city, i) => {
+      //     const { name, country, state, lat, lon } = city;
+      //     const row = document.createElement("tr");
+      //     row.innerHTML = `
+      //         <th scope="row">${++i}</th>
+      //         <td>${name}</td>
+      //         <td>${country}</td>
+      //         <td>${state ? state : ""}</td>
+      //   `;
+      //     row.setAttribute("data-lat", lat);
+      //     row.setAttribute("data-lon", lon);
+      //     table.append(row);
+      //   });
+
+      //   document.querySelector(".container").append(table);
+      console.log(data);
+
+      const { local_names, lat, lon } = data[0];
+
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=b473abf08211233160d13b09b0646297`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          data.localName = local_names.tr;
+          dataArr.push(data);
+          content.innerHTML = "";
+          dataArr.forEach((data) => {
+            const card = document.createElement("div");
+            card.className = "card shadow";
+            card.style.width = "18rem";
+            card.innerHTML = `
+          <div class="card-body">
+            <p class="card-text lead">${
+              data.localName ? data.localName : data.name
+            }</p>
+            <p class="card-text display-3"><strong class="fw-bold">${Math.trunc(
+              data.main.temp
+            )}</strong><small><sup>°C</sup></small></p>
+            <p class="card-text text-uppercase">${
+              data.weather[0].description
+            }</p>
+          </div>`;
+
+            document.querySelector(".content").append(card);
+          });
+        });
+    })
+    .catch((err) => console.log(err.message));
+});
+// const lat = 39.9207886;
+// const lon = 32.8540482;
+
+// fetch(
+//   "https://api.openweathermap.org/data/2.5/weather?lat=39.9207886&lon=32.8540482&units=metric&appid=b473abf08211233160d13b09b0646297"
+// )
+//   .then((res) => res.json())
+//   .then((data) => {
+//     // console.log(data);
+//     // console.log(data.main.temp);
+//     // console.log(data.weather[0].description);
+//     // console.log(data.weather[0].icon);
+//     // console.log(data.weather);
+
+//     const card = document.createElement("div");
+
+//     card.className = "card";
+//     card.style.width = "18rem";
+//     card.innerHTML = `
+// <div class="card-body">
+//   <p class="card-text">${data.name}</p>
+//   <p class="card-text">${data.main.temp}</p>
+//   <p class="card-text">${data.weather[0].description}</p>
+// </div>`;
+
+//     document.querySelector(".content").append(card);
+//   });
