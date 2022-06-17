eea562d535b36c2f086945dd172e80f2fc786ab8
erol1098 <erol1098@gmail.com>
2022-06-18 01:53:27 +0300

    Error Fixed

--- weather.js
+++ weather.js
@@ -40,8 +40,8 @@ const getWeather = async () => {
     );
     if (!res.ok) throw new Error("Something Went Wrong");
     const data = await res.json();
-    const { country, name, local_names, lat, lon } = data;
-    renderResult(data);
+    const { country, name, local_names, lat, lon } = data[0];
+
     let localeName;
     Object.entries(local_names).some((entry) => entry[0] === localLanguage)
       ? Object.entries(local_names).forEach((entry) => {
@@ -155,77 +155,9 @@ const convertLanguage = (language) => {
     dataArr.splice(0, dataArr.length);
   }
 };
+// function initialize() {
+//   var input = document.getElementById("searchTextField");
+//   new google.maps.places.Autocomplete(input);
+// }
 
-// const renderResult = (data) => {
-//   const cities = data.map((city) => {
-//     const { name, lat, lon, country, state } = city;
-//     return {
-//       name: name,
-//       country: country,
-//       state: state,
-//       lat: lat,
-//       lon: lon,
-//     };
-//   });
-//   console.log(cities);
-//   const table = document.createElement("table");
-//   table.classList.add("table");
-//   const title = document.createElement("thead");
-//   title.innerHTML = `
-//         <tr>
-//         <th scope="col">#</th>
-//         <th scope="col">City</th>
-//         <th scope="col">Country</th>
-//         <th scope="col">State</th>
-//       </tr>
-//       `;
-//   table.append(title);
-
-//   cities.forEach((city, i) => {
-//     const { name, country, state, lat, lon } = city;
-//     const row = document.createElement("tr");
-//     row.innerHTML = `
-//               <th scope="row">${++i}</th>
-//               <td>${name}</td>
-//               <td>${country}</td>
-//               <td>${state ? state : ""}</td>
-//         `;
-//     row.setAttribute("data-lat", lat);
-//     row.setAttribute("data-lon", lon);
-//     table.append(row);
-//   });
-
-//   document.querySelector(".container").append(table);
-// };
-// const renderResult = (data) => {
-//   const dropdown = document.querySelector(".dropdown-menu");
-//   const cities = data.map((city) => {
-//     const { name, lat, lon, country, state } = city;
-//     return {
-//       name: name,
-//       country: country,
-//       state: state,
-//       lat: lat,
-//       lon: lon,
-//     };
-//   });
-//   console.log(cities);
-
-//   cities.forEach((city) => {
-//     const row = document.createElement("li");
-//     row.classList.add("dropdown-item");
-//     const { name, country, state, lat, lon } = city;
-//     row.innerHTML = `${name} -${state ? state : ""}${country}`;
-//     row.setAttribute("data-lat", lat);
-//     row.setAttribute("data-lon", lon);
-//     row.dataset = `${name}`;
-//     dropdown.append(row);
-//   });
-// };
-
-function initialize() {
-  var input = document.getElementById("searchTextField");
-  new google.maps.places.Autocomplete(input);
-}
-
-google.maps.event.addDomListener(window, "load", initialize);
+// google.maps.event.addEventListener(window, "load", initialize);
