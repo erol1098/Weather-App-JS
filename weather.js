a8b122e3d069e12481e84018340fc0e643cac6e6
erol1098 <erol1098@gmail.com>
2022-06-18 02:35:45 +0300

    Error Fixed

--- weather.js
+++ weather.js
@@ -168,8 +168,8 @@ const convertLanguage = (language) => {
     dataArr.splice(0, dataArr.length);
   }
 };
-// function initialize() {
-//   var input = document.getElementById("searchTextField");
-//   new google.maps.places.Autocomplete(input);
-// }
-// google.maps.event.addDomListener(window, "load", initialize);
+function initialize() {
+  var input = document.getElementById("searchTextField");
+  new google.maps.places.Autocomplete(input);
+}
+google.maps.event.addDomListener(window, "load", initialize);
